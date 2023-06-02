f06431701f415c3d2dc5fa566f4e10c0abceea77
ROBINSON MOLINA <robinson.molina01@uptc.edu.co>
2023-06-01 20:42:39 -0500

    Agregando el metodo de resta al archivo 1 y suma al 2

--- archivo1.java
+++ archivo1.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class archivo1{
 
     public static void main(String[] args) {
@@ -5,5 +7,20 @@ public class archivo1{
         System.out.println("HOLA MUNDO");
 
         System.out.println("ACTUALIZANDO CAMBIOS");
+
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("Enter the firts number");
+        int num1 = input.nextInt();
+
+        System.out.println("Enter the second number");
+        int num2 = input.nextInt();
+
+        System.out.println("The result of substraction is: "+suma(num1, num2));
+    }
+
+    public static int suma(int num1, int num2){
+
+        return num1-num2;
     }
 }
--- archivo2.java
+++ archivo2.java
@@ -1,6 +1,23 @@
+import java.util.Scanner;
+
 public class archivo2 {
     public static void main(String[] args) {
         
         System.out.println("HOLA MUNDO DOS");
+
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("Enter the firts number");
+        int num1 = input.nextInt();
+
+        System.out.println("Enter the second number");
+        int num2 = input.nextInt();
+
+        System.out.println("The result of sum is: "+suma(num1, num2));
+    }
+
+    public static int suma(int num1, int num2){
+
+        return num1+num2;
     }
 }
